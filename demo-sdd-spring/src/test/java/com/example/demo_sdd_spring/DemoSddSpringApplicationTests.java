15482a9ffe09e6c30de5f72b481ebd690c714aaa
lofidewanto@gmail.com <lofidewanto@gmail.com>
2026-02-12 22:19:18 +0100

    Add demo-sdd-spring project

---
+++ demo-sdd-spring/src/test/java/com/example/demo_sdd_spring/DemoSddSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_sdd_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSddSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
